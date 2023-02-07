d5e23b7891af35f8b390fe8fdefe6f0fe4ed86ca
nomsopaul <kndominic@gmail.com>
2023-02-07 08:38:01 +0100

    google auth

--- server/models/User.js
+++ server/models/User.js
@@ -1,36 +1,37 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
         required: true,
         unique: true,
     },
     email: {
-        type:String,
+        type: String,
         required: true,
         unique: true,
     },
     password: {
         type: String,
-        required: true,
     },
     isAdmin: {
         type: Boolean,
         default: false,
     },
-    img:{
+    img: {
         type: String,
     },
-    subscribers:{
-        type:Number,
-        default:0
+    subscribers: {
+        type: Number,
+        default: 0
     },
-    subscribedUsers:{
-        type:[String]
-    }
-
-
+    subscribedUsers: {
+        type: [String]
+    },
+    fromGoogle:{
+        type: Boolean,
+        default: false,
+    },
 },
     { timestamps: true }
 );
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signin, signup } from "../controllers/auth.js";
+import { googleAuth, signin, signup } from "../controllers/auth.js";
 
 const router = express.Router();
 //create user
@@ -9,7 +9,7 @@ router.post("/signup", signup)
 router.post("/signin", signin)
 
 //google auth
-router.post("/google", )
+router.post("/google", googleAuth)
 
 
 export default router;
